19a6c406af0a306914af398b98dc12b30edcfd6d
Bonko <bonkowski.daniel@gmail.com>
2022-11-03 22:56:32 +0100

    wip: more tests

--- filehandler_test.go
+++ filehandler_test.go
@@ -1,13 +1,99 @@
 package main
 
 import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
 	"testing"
 )
+
+func Test_updateLastNumFileName(t *testing.T) {
+	expectedNumber := 5
+	file, err := os.CreateTemp("testdata", "updateLastNumFileName")
+	defer os.Remove(file.Name())
+	assert.NoError(t, err)
+
+	err = updateLastNumFileName(file.Name(), expectedNumber)
+	assert.NoError(t, err)
+
+	i, err := readIntFromFile(file.Name())
+	assert.NoError(t, err)
+	assert.Equal(t, expectedNumber, i)
+}
 
 func Test_readIntFromFile(t *testing.T) {
 	i, err := readIntFromFile("testdata/last_num_files_test")
 
 	assert.NoError(t, err)
 	assert.Equal(t, 1, i)
+}
+
+func Test_newFiles(t *testing.T) {
+	// TODO: implement
+	t.SkipNow()
+}
+
+func Test_genFileInfos(t *testing.T) {
+	// setup
+	dir := createTestdata()
+	defer os.RemoveAll(dir)
+
+	assert.Empty(t, filesByOwner)
+	err := filepath.WalkDir(dir, genFileInfos)
+	assert.NoError(t, err)
+
+	assert.NotEmpty(t, filesByOwner)
+	// TODO: more assertions
+
+}
+
+func createTestdata() string {
+	/*
+		testdata
+		├── folder1
+		│	├── 1.jpg
+		│	├── 2.jpg
+		│	└── 3.jpg
+		├── folder2
+		│	 ├── 1.jpg
+		│	 ├── 2.jpg
+		│	 └── 3.jpg
+		├── last_num_files_999
+		├── last_num_files_<current_user>
+	*/
+	tmp, err := os.MkdirTemp("", "testdata")
+	if err != nil {
+		log.Fatal(err)
+	}
+	testDir1 := fmt.Sprintf("%s/%s", tmp, "folder1")
+	testDir2 := fmt.Sprintf("%s/%s", tmp, "folder2")
+	files := []string{"1.jpg", "2.jpg", "3.jpg"}
+
+	for _, dir := range []string{testDir1, testDir2} {
+		err := os.Mkdir(dir, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, file := range files {
+			owner := os.Getuid()
+			if file == "file2.jpg" {
+				owner = 999
+			}
+			filename := fmt.Sprintf("%s/%s", dir, file)
+			_, err := os.Create(filename)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = os.Chown(filename, owner, os.Getgid())
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	// TODO: create last_num_.. files
+
+	return tmp
 }
